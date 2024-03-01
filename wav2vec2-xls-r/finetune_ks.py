fff49bf78c5d97c76e03635a10650357de016731
Oblivion <fengredrum@163.com>
2024-03-01 09:49:00 +0800

    1st worked version of keyword spotting

--- wav2vec2-xls-r/finetune_ks.py
+++ wav2vec2-xls-r/finetune_ks.py
@@ -16,9 +16,15 @@ if __name__ == "__main__":
     parser.add_argument("--max_duration_in_seconds", default=1.0, type=float)
     # Finetuning setups
     parser.add_argument("--gradient_accumulation_steps", default=2, type=int)
-    parser.add_argument("--train_batch_size", default=32, type=int)
-    parser.add_argument("--eval_batch_size", default=16, type=int)
+    parser.add_argument("--train_batch_size", default=64, type=int)
+    parser.add_argument("--eval_batch_size", default=32, type=int)
     parser.add_argument("--fp16", default=True, type=bool)
+
+    parser.add_argument("--warmup_steps", default=500, type=int)
+    parser.add_argument("--max_steps", default=10000, type=int)
+    parser.add_argument("--save_steps", default=1000, type=int)
+    parser.add_argument("--eval_steps", default=100, type=int)
+    parser.add_argument("--logging_steps", default=25, type=int)
 
     args = parser.parse_args()
     print(f"Settings: {args}")
@@ -56,7 +62,7 @@ if __name__ == "__main__":
 
     num_labels = len(id2label)
     model = AutoModelForAudioClassification.from_pretrained(
-        model_name,
+        args.model_name_or_path,
         num_labels=num_labels,
         label2id=label2id,
         id2label=id2label,
@@ -72,24 +78,29 @@ if __name__ == "__main__":
         return metric.compute(predictions=predictions, references=eval_pred.label_ids)
 
 
-    experiment_name = model_name.split("/")[-1]
+    experiment_name = args.model_name_or_path.split("/")[-1]
 
     args = TrainingArguments(
-        f"{model_name}-finetuned-ks",
-        evaluation_strategy="epoch",
-        save_strategy="epoch",
+        f"logs/{args.model_name_or_path}-finetuned-ks",
         learning_rate=3e-5,
-        per_device_train_batch_size=64,
-        gradient_accumulation_steps=4,
-        per_device_eval_batch_size=32,
-        num_train_epochs=5,
-        warmup_ratio=0.1,
-        logging_steps=10,
-        eval_steps=10,
+        warmup_steps=args.warmup_steps,
+        per_device_train_batch_size=args.train_batch_size,
+        per_device_eval_batch_size=args.eval_batch_size,
+        gradient_accumulation_steps=args.gradient_accumulation_steps,
+        evaluation_strategy="steps",
+        max_steps=args.max_steps,
+        gradient_checkpointing=True,
+        fp16=args.fp16,
+        save_steps=args.save_steps,
+        eval_steps=args.eval_steps,
+        logging_steps=args.logging_steps,
+        save_total_limit=2,
+        report_to=["tensorboard"],
         load_best_model_at_end=True,
-        metric_for_best_model="accuracy",
-        fp16=True,
+        metric_for_best_model=args.metric,
+        greater_is_better=False,
         push_to_hub=False,
+        group_by_length=True,
     )
 
     trainer = Trainer(
